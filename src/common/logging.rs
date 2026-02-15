4497519311b798c9180568abf2e4690126b76bc7
Regev Myers <regev.myers@gmail.com>
2026-02-15 20:13:06 +0200

    Lo Echpat li

--- src/common/logging.rs
+++ src/common/logging.rs
@@ -46,18 +46,18 @@ fn to_symbol(log_type: Type) -> char {
     }
 }
 
-pub fn ok(message: &str) { log(Type::Ok, message) }
-pub fn unclear(message: &str) { log(Type::Unclear, message) }
-pub fn fail(message: &str) { log(Type::Fail, message) }
-pub fn error(message: &str) { log(Type::Error, message) }
-pub fn fatal(message: &str) { log(Type::Fatal, message) }
-pub fn location(message: &str) { log(Type::Location, message) }
-pub fn parrallel(message: &str) { log(Type::Parrallel, message) }
-pub fn info(message: &str) { log(Type::Info, message) }
-pub fn command(message: &str) { log(Type::Command, message) }
-pub fn callback(message: &str) { log(Type::Callback, message) }
-pub fn hint(message: &str) { log(Type::Hint, message) }
-pub fn comment(message: &str) { log(Type::Comment, message) }
-pub fn input(message: &str) { log(Type::Input, message) }
-pub fn output(message: &str) { log(Type::Output, message) }
+#[allow(unused)] pub fn ok(message: &str) { log(Type::Ok, message) }
+#[allow(unused)] pub fn unclear(message: &str) { log(Type::Unclear, message) }
+#[allow(unused)] pub fn fail(message: &str) { log(Type::Fail, message) }
+#[allow(unused)] pub fn error(message: &str) { log(Type::Error, message) }
+#[allow(unused)] pub fn fatal(message: &str) { log(Type::Fatal, message) }
+#[allow(unused)] pub fn location(message: &str) { log(Type::Location, message) }
+#[allow(unused)] pub fn parrallel(message: &str) { log(Type::Parrallel, message) }
+#[allow(unused)] pub fn info(message: &str) { log(Type::Info, message) }
+#[allow(unused)] pub fn command(message: &str) { log(Type::Command, message) }
+#[allow(unused)] pub fn callback(message: &str) { log(Type::Callback, message) }
+#[allow(unused)] pub fn hint(message: &str) { log(Type::Hint, message) }
+#[allow(unused)] pub fn comment(message: &str) { log(Type::Comment, message) }
+#[allow(unused)] pub fn input(message: &str) { log(Type::Input, message) }
+#[allow(unused)] pub fn output(message: &str) { log(Type::Output, message) }
 
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
-use std::fs::File;
-use std::io::{BufReader, Read};
-use std::path::Path;
-
 mod sgf;
 mod common;
 
 use sgf::parser::parsers::collection::Collection;
 use common::logging as log;
 
+use std::path::Path;
+
+use std::fs;
+use std::io;
+use std::io::{Read};
+use std::env;
+
 fn read_file(path: &Path) -> Result<String, std::io::Error> {
-    let mut reader = BufReader::new(File::open(&path)?);
+    let mut reader = io::BufReader::new(fs::File::open(&path)?);
     let mut content = String::new();
     reader.read_to_string(&mut content)?;
 
@@ -18,7 +21,9 @@ fn read_file(path: &Path) -> Result<String, std::io::Error> {
 
 fn main() -> Result<(), common::error::Error> {
     log::location("Main");
+    log::input(&format!("Args: {:?}", env::args().collect::<Vec<_>>()));
 
+    log::location(&format!("Reading: {}", env::args().next().unwrap()));
     let c: Collection = "wajawaja".parse()?;
     // let content = read_file(Path::new("ff4_ex.sgf"))?;
     dbg!(c);
